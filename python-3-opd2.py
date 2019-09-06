1a9e04d572eda3952d3772cd7735679d86177279
Dorien <36191258+Dorien2005@users.noreply.github.com>
2019-09-06 11:47:22 +0200

    Update python-3-opd2.py

--- python-3-opd2.py
+++ python-3-opd2.py
@@ -1,11 +1,10 @@
-def iets(x):
-    if x == 8:
-        return True
+def hoogste_van_drie(x):
+    if x[0] > x[1] and x[0]>x[2]:
+        print(x[0])
+    elif x[1] > x[0] and x[1] > x[2]:
+        print(x[1])
     else:
-        return False
+        print(x[2])
+        
     
-print(iets(8))
-
-
-
-    
+hoogste_van_drie([4,2,9])
