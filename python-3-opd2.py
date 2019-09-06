c567b001aa8dc2df6db43e247d46323964fcde4e
Dorien <36191258+Dorien2005@users.noreply.github.com>
2019-09-06 12:01:57 +0200

    Update python-3-opd2.py

--- python-3-opd2.py
+++ python-3-opd2.py
@@ -1,10 +1,5 @@
-def hoogste_van_drie(x):
-    if x[0] > x[1] and x[0]>x[2]:
-        print(x[0])
-    elif x[1] > x[0] and x[1] > x[2]:
-        print(x[1])
-    else:
-        print(x[2])
-        
+def omdraaien(x):
+    return x[::-1]
     
-hoogste_van_drie([4,2,9])
+    
+print(omdraaien("modus"))
