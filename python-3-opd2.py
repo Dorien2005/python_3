8e254ce240a88e9ea0b661111b55e61cfe537f62
Dorien <36191258+Dorien2005@users.noreply.github.com>
2019-09-06 12:42:51 +0200

    Update python-3-opd2.py

--- python-3-opd2.py
+++ python-3-opd2.py
@@ -1,5 +1,12 @@
-def omdraaien(x):
-    return x[::-1]
-    
-    
-print(omdraaien("modus"))
+def is_priemgetal(num):
+    if num > 1:
+        for i in range(2,num):
+           if (num % i) == 0:
+               print(num,"is geen priemgetal")
+               break
+        else:
+           print(num,"is een priemgetal")
+       
+    else:
+       print(num,"is geen priemgetal")       
+is_priemgetal(23)
