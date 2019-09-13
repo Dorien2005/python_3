514134e96e2e8eb342d2b23243a1d306dffeb6dc
Dorien <36191258+Dorien2005@users.noreply.github.com>
2019-09-13 11:31:27 +0200

    Update pyton3opd.py

--- pyton3opd.py
+++ pyton3opd.py
@@ -1,5 +1,8 @@
-def boek(numbers, key):
-    del numbers[key]
-    return numbers
-
-print(boek({1:2,2:3,3:4,4:5,5:6}, 5))
+def boek(numbers):
+    maximum = max(numbers.values())
+    minimum = min(numbers.values())
+    lijst = [maximum, minimum]
+    return lijst
+    #return minimum
+    
+print(boek({1:2,2:3,3:4,4:5,5:6}))
