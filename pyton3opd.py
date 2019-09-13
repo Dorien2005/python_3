e54722b613a9798d34e805c824021734eaee0d45
Dorien <36191258+Dorien2005@users.noreply.github.com>
2019-09-13 11:39:53 +0200

    Update pyton3opd.py

--- pyton3opd.py
+++ pyton3opd.py
@@ -1,8 +1,14 @@
+def copy(dict):
+    dictionary = dict.copy()
+    return dictionary
+    
+print(copy({1,2,3,4,5}))
+
+
 def boek(numbers):
     maximum = max(numbers.values())
     minimum = min(numbers.values())
     lijst = [maximum, minimum]
     return lijst
-    #return minimum
-    
+
 print(boek({1:2,2:3,3:4,4:5,5:6}))
